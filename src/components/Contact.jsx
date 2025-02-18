47f11bcb81680d7415b7c13093292b0dcba43b36
Aditya Bhalerao <adityab7968@gmail.com>
2025-02-19 01:32:20 +0530

    New theme

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,10 +16,11 @@ export default function ContactSection() {
                     transition={{ duration: 0.5 }}
                     className="text-center text-5xl md:text-7xl"
                 >
-                    <span className="bg-gradient-to-r from-indigo-500 to-blue-500 bg-clip-text text-transparent">
+                    <span className="bg-gradient-to-r from-indigo-400 to-purple-500 bg-clip-text text-transparent">
                         Let's Connect!
                     </span>
                 </motion.h1>
+
                 <motion.div
                     variants={variants}
                     initial="hidden"
@@ -28,11 +29,12 @@ export default function ContactSection() {
                     className="flex flex-col items-center space-y-8 text-center"
                 >
                     <p className="text-lg text-gray-300 font-semibold">
-                        I'm always open to new opportunities, collaborations, or just a friendly chat! Drop me a message and I'll get back to you as soon as I can.
+                        I'm always open to new opportunities, collaborations, or just a friendly chat! Drop me a message, and I'll get back to you as soon as I can.
                     </p>
+
                     <a
                         href="mailto:adityab7968@gmail.com"
-                        className="text-nowrap rounded-lg border border-indigo-600 bg-black px-5 py-3 text-lg font-bold text-white shadow-lg shadow-indigo-700 transition-all duration-300 hover:-translate-y-2 hover:shadow-xl hover:shadow-indigo-600"
+                        className="rounded-lg border border-indigo-600 bg-[#111132] px-5 py-3 text-lg font-bold text-white shadow-lg shadow-indigo-900 transition-all duration-300 hover:-translate-y-2 hover:shadow-xl hover:shadow-purple-700"
                     >
                         Reach Out Now
                     </a>
