2310af972c1cabb7382a30dccc99e04309d443f1
Aditya Bhalerao <adityab7968@gmail.com>
2025-03-03 10:48:46 +0530

    Add contribution

--- src/data/ossData.js
+++ src/data/ossData.js
@@ -4,7 +4,8 @@ const contributions = [
         link: "https://github.com/redis/rueidis",
         summary: "A fast Golang Redis client that supports Client Side Caching, Auto Pipelining, Generics OM, RedisJSON, RedisBloom, RediSearch, etc.",
         prs: [
-            { kind: "[PR]", title: "Implemented LCS (Longest Common Substring) Command", link: "https://github.com/redis/rueidis/pull/767" },
+            { kind: "[PR/Feature]", title: "Implemented LCS (Longest Common Substring) Command", link: "https://github.com/redis/rueidis/pull/767" },
+            { kind: "[PR/Bug]", title: "Ensure AUTH Command is Sent Before HELLO in NewClient Initialization", link: "https://github.com/redis/rueidis/pull/791" },
         ],
     },
     {
@@ -12,7 +13,7 @@ const contributions = [
         link: "https://github.com/DiceDB/dice",
         summary: "DiceDB is an open-source in-memory database with query subscriptions.",
         prs: [
-            { kind: "[PR]", title: "Add APPEND Command to DiceDB with Redis-like Behavior, Tests, and Benchmarking", link: "https://github.com/DiceDB/dice/pull/759" },
+            { kind: "[PR/Feature]", title: "Add APPEND Command to DiceDB with Redis-like Behavior, Tests, and Benchmarking", link: "https://github.com/DiceDB/dice/pull/759" },
             { kind: "[Issue]", title: "Reported Inconsistent MSET Command Behavior", link: "https://github.com/DiceDB/dice/issues/516" },
         ],
     },
